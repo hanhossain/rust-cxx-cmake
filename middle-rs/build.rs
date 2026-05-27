c4a2d1ac8dbe8aff837fcce7b2535ca82e75251e
Han Hossain <hanhossain@outlook.com>
2026-05-27 00:25:58 -0500

    consume with cmake

--- middle-rs/build.rs
+++ middle-rs/build.rs
@@ -1,11 +1,10 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
         .include("../middle-cpp/include")
-        .std("c++17")
-        .file("../middle-cpp/MiddleCpp.cpp")
         .compile("middle-rs-binding");
-    // let dst = cmake::Config::new("..").build();
-    // println!("cargo::rustc-link-search=native={}/lib", dst.display());
+    let dst = cmake::Config::new("..").build();
+    println!("cargo::rustc-link-search=native={}/lib", dst.display());
+    println!("cargo::rustc-link-lib=middle-cpp");
     println!("cargo::rerun-if-changed=../middle-cpp/");
     println!("cargo::rerun-if-changed=../CMakeLists.txt");
 }
